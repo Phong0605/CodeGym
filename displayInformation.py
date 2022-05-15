7da7d33c869080509a6350aa00d285b2864bf0a2
Thanh Phong <95267305+Phong0605@users.noreply.github.com>
2022-05-15 23:29:49 +0700

    Create displayInformation.py

---
+++ displayInformation.py
@@ -0,0 +1,4 @@
+print("Trần Thanh Phong")
+print("Ngày sinh: 06/05/2003")
+print("Địa chỉ: Số 26, đường Đoàn Thị Điểm, khu phố 2, phường Phú Lâm, TP Tuy Hòa, tỉnh Phú Yên.")
+print("Mục tiêu sau khóa học: Thành thạo được kĩ năng Python căn bản để giúp ích cho sau này.")
